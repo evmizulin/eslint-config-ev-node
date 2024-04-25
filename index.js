1a6ee9f4d381060a275f57ca6ef285d19ff23666
Evgeny Mizulin <evgeny.mizulin@gmail.com>
2024-04-25 20:07:41 +0300

    ts config

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 module.exports = {
   env: {
     browser: false,
-    es6: true,
+    es2021: true,
     node: true,
   },
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "tsconfig.json",
-    sourceType: "module",
-    ecmaVersion: 2020,
   },
   plugins: ["@typescript-eslint", "prettier"],
   extends: [
